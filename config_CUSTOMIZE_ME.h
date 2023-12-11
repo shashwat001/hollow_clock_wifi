6d6a0a4e8123705ee8911dd72df890ef333f0db1
shashwat001 <shashwatkmr.001@gmail.com>
2023-12-11 12:14:47 +0530

    Adding example config.h file

---
+++ config_CUSTOMIZE_ME.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Replace with your actual SSID and password:
+#define WIFI1_SSID "***"
+#define WIFI1_PASSWD "***"
+
+#define WIFI2_SSID "***"
+#define WIFI2_PASSWD "***"
+
+#endif
